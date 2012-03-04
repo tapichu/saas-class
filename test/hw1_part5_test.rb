f8b91f8cab703323e936ca6f1b18f73c83ecbcea
Meck <yesmeck@gmail.com>
2012-03-04 21:51:30 +0800

    Add multiple attr_accessor_with_history test to part5

--- test/hw1_part5_test.rb
+++ test/hw1_part5_test.rb
@@ -5,9 +5,14 @@ class Foo
     attr_accessor_with_history :bar
 end
 
+class Bar
+    attr_accessor_with_history :foo
+    attr_accessor_with_history :bar
+end
+
 class TestAdvancedOOP < Test::Unit::TestCase
 
-    def test_attr_accessor_with_history
+    def test_single_attr_accessor_with_history
         f = Foo.new
         assert_not_nil f
 
@@ -18,7 +23,7 @@ class TestAdvancedOOP < Test::Unit::TestCase
         assert_equal [nil, 3, :wowzo, 'boo!'], f.bar_history
     end
 
-    def test_attr_accessor_with_history_different_instances
+    def test_single_attr_accessor_with_history_different_instances
         f = Foo.new
         f.bar = 1
         f.bar = 2
@@ -26,6 +31,34 @@ class TestAdvancedOOP < Test::Unit::TestCase
         f.bar = 4
 
         assert_equal [nil, 4], f.bar_history
+    end
+
+    def test_multiple_attr_accessor_with_history
+        b = Bar.new
+        b.bar = 3
+        b.bar = :wowzo
+        b.bar = 'boo!'
+        b.foo = 4
+        b.foo = :woozy
+        b.foo = 'boom!'
+
+        assert_equal [nil, 3, :wowzo, 'boo!'], b.bar_history
+        assert_equal [nil, 4, :woozy, 'boom!'], b.foo_history
+    end
+
+    def test_multiple_attr_accessor_with_history_different_instances
+        b = Bar.new
+        b.bar = 1
+        b.bar = 2
+        b.foo = 3
+        b.foo = 4
+
+        b = Bar.new
+        b.bar = 4
+        b.foo = 5
+
+        assert_equal [nil, 4], b.bar_history
+        assert_equal [nil, 5], b.foo_history
     end
 
 end
