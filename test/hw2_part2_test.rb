6e6050d6e8124c4dbd8fabe905ca17a260646ccd
Brent J. Nordquist <brent@nordist.net>
2012-03-07 22:47:53 -0600

    Test with Enumerables that aren't an Array

--- test/hw2_part2_test.rb
+++ test/hw2_part2_test.rb
@@ -23,4 +23,14 @@ class TestCartesianProduct < Test::Unit::TestCase
     assert_equal 0, CartesianProduct.new([:a,:b], []).count
   end
 
+  def test_cartesian_product_of_ranges
+    expected_result = [[2,3],[2,4],[2,5],[2,6],
+                       [3,3],[3,4],[3,5],[3,6],
+                       [4,3],[4,4],[4,5],[4,6]]
+    product = CartesianProduct.new((2..4), (3..6))
+
+    assert_equal expected_result.count, product.count
+    assert expected_result.all? { |elt| product.include? elt }
+  end
+
 end
